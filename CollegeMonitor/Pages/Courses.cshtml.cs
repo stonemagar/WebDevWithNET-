8f59150587fedc97de6c77fa998c02d48c9fd40d
Stone <stonemagar63@gmail.com>
2024-05-02 18:04:48 +0545

    Configue Courses.cshtml.cs

---
+++ CollegeMonitor/Pages/Courses.cshtml.cs
@@ -0,0 +1,14 @@
+
+using Microsoft.AspNetCore.Mvc.RazorPages;
+public class CoursesModel : PageModel
+
+
+{
+    public List<Course> Courses { get ; set ;}
+    public void OnGet()
+    {
+        CollegeDbContext db = new();
+        Courses= db.Courses.ToList();
+
+    }
+}
